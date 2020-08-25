e579046a348fbd4c301cd3541df42cb4027d8954
aterenin <aterenin@users.noreply.github.com>
2020-08-25 13:30:53 +0100

    Reordered batching dimensions for readability.

--- sparsegpax/gp.py
+++ sparsegpax/gp.py
@@ -5,6 +5,7 @@ import jax.random as jr
 import jax.scipy as jsp
 import jax
 import tensorflow_probability
+from tensorflow_probability.python.internal.backend import jax as tf2jax
 tfp = tensorflow_probability.experimental.substrates.jax
 tfk = tfp.math.psd_kernels
 from sparsegpax.spectral import standard_spectral_measure, spectral_weights
@@ -42,11 +43,11 @@ class SparseGaussianProcess:
         (L,ID,OD,M,S) = (self.num_basis ,self.input_dimension, self.output_dimension, self.num_inducing, self.num_samples)
         self.prior_frequency = jnp.zeros((OD, ID, L))
         self.prior_phase = jnp.zeros((OD, L))
-        self.prior_weights = jnp.zeros((OD, L, S))
+        self.prior_weights = jnp.zeros((S, OD, L))
         self.inducing_locations = jr.normal(key, (M, ID))
         self.inducing_pseudo_mean = jnp.zeros((OD, M))
         self.inducing_pseudo_log_errvar = jnp.ones((OD, M))
-        self.inducing_weights = jnp.zeros((OD, M, S))
+        self.inducing_weights = jnp.zeros((S, OD, M))
         self.cholesky = jnp.zeros((OD, M, M))
         
         self.resample_prior_basis(key)
@@ -61,14 +62,14 @@ class SparseGaussianProcess:
         Args:
             x: the input matrix.
         """
-        (ID,OD,M,S) = (self.input_dimension, self.output_dimension, self.num_inducing, self.num_samples)
+        (S,OD,ID,M) = (self.num_samples, self.output_dimension, self.input_dimension, self.num_inducing)
         # inducing_locations = hk.get_parameter("inducing_locations", [M,ID], init=hk.initializers.RandomUniform())
-        # inducing_weights = hk.get_state("inducing_weights", [OD,M,S], init=jnp.zeros)
+        # inducing_weights = hk.get_state("inducing_weights", [S,OD,M], init=jnp.zeros)
         inducing_locations = self.inducing_locations
         inducing_weights = self.inducing_weights
 
         f_prior = self.prior(x)
-        f_data = (self.kernel.matrix(x, inducing_locations) @ inducing_weights).T # non-batched
+        f_data = tf2jax.linalg.matvec(self.kernel.matrix(x, inducing_locations), inducing_weights) # non-batched
 
         (N,ID) = x.shape
         (OD,M,S) = inducing_weights.shape
@@ -94,7 +95,7 @@ class SparseGaussianProcess:
 
         key_prior_weights, key_residual = jr.split(key)
 
-        (S,M,OD,ID,L) = (self.num_samples, self.num_inducing, self.output_dimension, self.input_dimension, self.num_basis)
+        (S,OD,ID,M,L) = (self.num_samples, self.output_dimension, self.input_dimension, self.num_inducing, self.num_basis)
         # inducing_locations = hk.get_parameter("inducing_locations", [M,ID], init=hk.initializers.RandomUniform())
         # inducing_pseudo_mean = hk.get_parameter("inducing_pseudo_mean", [OD,M], init=jnp.zeros)
         # inducing_pseudo_log_errvar = hk.get_parameter("inducing_pseudo_log_errvar", [OD,M], init=jnp.zeros)
@@ -102,28 +103,28 @@ class SparseGaussianProcess:
         inducing_pseudo_mean = self.inducing_pseudo_mean
         inducing_pseudo_log_errvar = self.inducing_pseudo_log_errvar
 
-        prior_weights = jr.normal(key_prior_weights, (OD,L,S))
+        prior_weights = jr.normal(key_prior_weights, (S,OD,L))
         # hk.set_state("prior_weights", prior_weights)
         self.prior_weights = prior_weights
 
-        (cholesky,_) = jsp.linalg.cho_factor(self.kernel.matrix(inducing_locations, inducing_locations) + jax.vmap(jnp.diag)(jnp.exp(inducing_pseudo_log_errvar)))
-        residual = self.prior(inducing_locations).T - (jnp.reshape(jnp.exp(inducing_pseudo_log_errvar / 2), (OD,M,1)) * jr.normal(key_residual,(OD,M,S))) # TODO: careful with f32!
-        inducing_weights = jnp.reshape(inducing_pseudo_mean,(OD,M,1)) + jsp.linalg.cho_solve((cholesky,False),residual) # mean-reparameterized v = \mu + (K + V)^{-1}(f - \eps)
+        (cholesky,_) = jsp.linalg.cho_factor(self.kernel.matrix(inducing_locations, inducing_locations) + jax.vmap(jnp.diag)(jnp.exp(inducing_pseudo_log_errvar)), lower=True)
+        residual = self.prior(inducing_locations) - jnp.exp(inducing_pseudo_log_errvar / 2) * jr.normal(key_residual,(S,OD,M)) # TODO: careful with f32!
+        inducing_weights = inducing_pseudo_mean + tf2jax.linalg.LinearOperatorLowerTriangular(cholesky).solvevec(residual) # mean-reparameterized v = \mu + (K + V)^{-1}(f - \eps)
 
         # hk.set_state("inducing_weights", inducing_weights)
         # hk.set_state("cholesky", cholesky)
         self.inducing_weights = inducing_weights
         self.cholesky = cholesky
         
-        assert prior_weights.shape == (OD,L,S)
+        assert prior_weights.shape == (S,OD,L)
         (M,ID) = inducing_locations.shape
-        (OD2,M2,S) = self.inducing_weights.shape
+        (S,OD2,M2) = self.inducing_weights.shape
         assert M==M2
         assert OD==OD2
         assert cholesky.shape==(OD,M,M)
-        assert self.prior(inducing_locations).shape==(S,M,OD)
-        assert residual.shape==(OD,M,S)
-        assert inducing_weights.shape==(OD,M,S)
+        assert self.prior(inducing_locations).shape==(S,OD,M)
+        assert residual.shape==(S,OD,M)
+        assert inducing_weights.shape==(S,OD,M)
 
 
     def resample_prior_basis(
@@ -173,7 +174,7 @@ class SparseGaussianProcess:
         (S,OD,ID,L) = (self.num_samples, self.output_dimension, self.input_dimension, self.num_basis)
         # prior_frequency = hk.get_state("prior_frequency", [OD,ID,L], init=jnp.zeros)
         # prior_phase = hk.get_state("prior_phase", [OD,L], init=jnp.zeros)
-        # prior_weights = hk.get_state("prior_weights", [OD,L,S], init=jnp.zeros)
+        # prior_weights = hk.get_state("prior_weights", [S,OD,L], init=jnp.zeros)
         prior_frequency = self.prior_frequency
         prior_phase = self.prior_phase
         prior_weights = self.prior_weights
@@ -181,29 +182,24 @@ class SparseGaussianProcess:
         (outer_weights, inner_weights) = spectral_weights(self.kernel, prior_frequency)
         rescaled_x = x / inner_weights
         basis_fn_inner_prod = rescaled_x @ prior_frequency
-        basis_fn = jnp.cos(basis_fn_inner_prod + jnp.reshape(prior_phase, (OD,1,L)))
-        basis_weight = jnp.sqrt(2/L) * jnp.reshape(outer_weights,(OD,L,1)) * prior_weights # TODO: typecast this to f32!
-        output = (basis_fn @ basis_weight).T
+        basis_fn = jnp.cos(basis_fn_inner_prod + jnp.expand_dims(prior_phase,-2))
+        basis_weight = jnp.sqrt(2/L) * outer_weights * prior_weights
+        output = tf2jax.linalg.matvec(basis_fn, basis_weight)
 
         (OD,ID,L) = prior_frequency.shape
         (OD2,L2) = prior_phase.shape
         (N,ID2) = x.shape
-        (OD3,L3,S) = prior_weights.shape
+        (S,OD3,L3) = prior_weights.shape
         (OD4,L4) = outer_weights.shape
         (ID3,) = inner_weights.shape
-        assert L==L2
-        assert L==L3
-        assert L==L4
-        assert ID==ID2
-        assert OD==OD2
-        assert ID==ID3
-        assert OD==OD3
-        assert OD==OD4
-        assert basis_weight.shape == (OD,L,S)
+        assert L==L2==L3==L4
+        assert ID==ID2==ID3
+        assert OD==OD2==OD3==OD4
+        assert basis_weight.shape == (S,OD,L)
         assert basis_fn.shape == (OD,N,L)
         assert basis_fn_inner_prod.shape == (OD,N,L)
         assert rescaled_x.shape == (N,ID)
-        assert output.shape == (S,N,OD)
+        assert output.shape == (S,OD,N)
 
         return output
 
@@ -226,7 +222,7 @@ class SparseGaussianProcess:
 
         logdet_term = (2 * jnp.sum(jax.vmap(jnp.diag)(cholesky))) - jnp.sum(inducing_pseudo_log_errvar)
         kernel_matrix = self.kernel.matrix(inducing_locations, inducing_locations)
-        cholesky_inv = jax.vmap(lambda x: jsp.linalg.cho_solve((x,False), jnp.eye(*x.shape)))(cholesky)
+        cholesky_inv = tf2jax.linalg.LinearOperatorLowerTriangular(cholesky).solve(jnp.eye(cholesky.shape[-1]))
         trace_term = jnp.sum(cholesky_inv * kernel_matrix)
         reparameterized_quadratic_form_term = jnp.sum(inducing_pseudo_mean @ kernel_matrix @ inducing_pseudo_mean.T)
         return (logdet_term - (OD*ID*M) + trace_term + reparameterized_quadratic_form_term) / 2
